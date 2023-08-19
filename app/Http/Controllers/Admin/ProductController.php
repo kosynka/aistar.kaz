22706a9753806a6f347b3e96dc66480d115ec86a
china <s.zhumabay@list.ru>
2023-08-20 04:04:06 +0600

    edit 2 controllers

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,71 +3,61 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
         $products = Product::all();
+
         return view('admin.products.index', compact('products'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        return view('admin.products.create');
+        return view('admin.products.create',['title'=>_('admin.title.products.create')];
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        Product::create($request->all());
-        return redirect()->route('admin.products.index');
+        Product::create($request->validate($this->rules));
+
+        return redirect()->route('admin.products.index')->with(['succes'=>_('admin.success.create')]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function edit(Product $product)
     {
-        $product = Product::findOrFail($id);
-        return view('admin.products.show', compact('product'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $product = Product::findOrFail($id);
         return view('admin.products.edit', compact('product'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Product $product, Request $request)
     {
-        $product = Product::findOrFail($id);
-        $product->update($request->all());
-        return redirect()->route('admin.products.index');
+        $product->update($request->validate($this->rules));
+
+        return redirect()->route('admin.products.index')->with(['success' => _('admin.success.update')]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        $product = Product::findOrFail($id);
         $product->delete();
-        return redirect()->route('admin.products.index');
+
+        return redirect()->route('admin.products.index')->with(['success' => _('admin.success.destroy')]);
     }
+
+    private array $rules = [
+        'name' => ['required', 'string', 'min:1'],
+        'description' => ['required', 'string', 'min:1'],
+        'price' => ['required', 'numeric', 'min:1'],
+        'discount_price' => ['required', 'nullable', 'numeric', 'min:1'],
+        'amount' => ['required', 'integer', 'min:1'],
+        'category_id' => ['required', 'integer', 'min:1', 'exists:categories,id'],
+        'relevance_weight' => ['required','nullable', 'integer', 'min:1'],
+        'rating' => ['required', 'integer', 'nullable', 'min:1'],
+        'prosklad_id' => ['required', 'integer', 'min:1', 'exists:categories,id'],
+        'views_count' => ['required', 'integer', 'nullable', 'min:1'],
+        'views_count' => ['required', 'string', 'unique', 'min:1'],
+    ];
 }
